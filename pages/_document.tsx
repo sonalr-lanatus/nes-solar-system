e0d43ebd309f19536a177e8243117df6fc39695b
sonalr-lanatus <sonal.r@lanatussystems.com>
2024-01-31 15:07:39 +0530

    add google tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,8 +4,28 @@ import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
 export default function Document() {
   return (
     <Html>
-      <Head></Head>
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NSKJG5KT');`,
+          }}
+        ></script>
+      </Head>
       <body>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NSKJG5KT"
+            height="0"
+            width="0"
+            style="display:none;visibility:hidden"
+          ></iframe>`,
+          }}
+        />
         <Main />
         <NextScript />
         <GoogleTagManager gtmId="G-7NNJFP05M4" />{" "}
