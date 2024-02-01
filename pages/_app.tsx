34bf19948461a3fbd16dafcc6bfa9ffb9c73e7dc
sonalr-lanatus <sonal.r@lanatussystems.com>
2024-02-01 11:54:38 +0530

    add hotjar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
 import type { AppProps } from "next/app";
+import { hotjar } from "react-hotjar";
+import { useEffect } from "react";
 import "../pages/globals.css";
 import "../pages/SolarPlanningInstallation/SolarPlanningInstallation.css";
 import "../pages/OurTeam/OurTeam.css";
@@ -7,6 +9,9 @@ import "../pages/Projects/CivilWork/CivilWork.css";
 import "../pages/Awards/Awards.css";
 import RootLayout from "@/components/Layout";
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    hotjar.initialize(3848934, 6);
+  }, []);
   return (
     <>
       <RootLayout>
